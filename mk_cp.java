4771d12bdaf03be2a236954ba00f62ddff726a05
flyntga <austin.flynt@jacobs.com>
2019-06-14 11:05:16 -0500

    commented cp plane generator

--- mk_cp.java
+++ mk_cp.java
@@ -16,9 +16,11 @@ public class mk_cp extends StarMacro {
 
     Simulation sim = getActiveSimulation();
 
+    // units
     Units um = ((Units) sim.getUnitsManager().getObject("m"));
     Units uf = ((Units) sim.getUnitsManager().getObject("ft"));
 
+    // reference plane to copy general settings
     ConstrainedPlaneSection rcp = ((ConstrainedPlaneSection) sim.getPartManager().getObject("cp_u_x[+00]")); //reference cp
 
     double r0 = 0.0;
@@ -32,10 +34,12 @@ public class mk_cp extends StarMacro {
     int i, j;
 
 
+    // iterate over x
     for ( i=-14; i < 12; i++){
 
       x = (i)*dx;
 
+      // generate a string name based on position
       String sn = "cp_u_x[";
       String sx = String.format("%03.0f", x);
       String name = sn + sx + "]";
@@ -43,11 +47,15 @@ public class mk_cp extends StarMacro {
       System.out.println(name + " = " + x);
 
 
+      // make a new constrained plane
       ConstrainedPlaneSection ncp = (ConstrainedPlaneSection) sim.getPartManager().createConstrainedPlaneImplicitPart(new NeoObjectVector(new Object[] {}), new DoubleVector(new double[] {}), um);
       ncp.copyProperties(rcp);
       ncp.setReevaluateStatus(true);
 
+      // rename the plane
       ncp.setPresentationName(name);
+
+      // set the x coordinate
       ncp.getOriginCoordinate().setCoordinate(uf, uf, uf, new DoubleVector(new double[] {x, 0.0, 0.0}));
     }
   }
